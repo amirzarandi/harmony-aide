c6d2c4e2434266e028ff85717a748849d443faae
Amir Zarandi <amir.h.zarandi@lawrence.edu>
2023-09-17 09:02:25 -0400

    Full transcription enabled

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,55 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import "./App.css";
-import { AudioRecorder, VideoRecorder } from './record';
 
 const baseUrl = "http://127.0.0.1:5000";
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US'
 
 function App() {
   const [msg, setMsg] = useState("");
   const [convosList, setConvosList] = useState([]);
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const[savedNotes, setSavedNotes] = useState([])
+  const [transcript, setTranscript] = useState("")
+
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if(isListening) {
+      mic.start()
+      mic.onend = () => {
+        console.log('continue..')
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('Stopped Mic on Click')
+        console.log(transcript)
+        handleSubmit()
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mics on')
+    }
+
+    mic.onresult = event => {
+      setTranscript(Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript).join(''))
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
 
   const fetchConvos = async () => {
     const data = await axios.get(`${baseUrl}/convos`)
@@ -20,12 +62,10 @@ function App() {
     setMsg(e.target.value);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     try {
-
-      const reply = await axios.post(`${baseUrl}/response`, { 'input': msg });
-      const data1 = await axios.post(`${baseUrl}/convos`, { 'username': 'Deloris', msg})
+      const reply = await axios.post(`${baseUrl}/response`, { 'input': transcript });
+      const data1 = await axios.post(`${baseUrl}/convos`, { 'username': 'Deloris', 'msg': transcript})
       const data2 = await axios.post(`${baseUrl}/convos`, { 'username': 'AI', 'msg': reply['data']})
       setMsg('');
       const dat3 = await axios.post(`${baseUrl}/tts`, { 'text': reply['data'] })
@@ -42,6 +82,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="container">
+          <div className="box">
+            {isListening ? <span>🎤</span> : <span>🔴🎤</span>}
+            <button onClick={() => setIsListening(prevState => !prevState)}>Start/Stop</button>
+          </div>
+        </div>
         <form onSubmit={handleSubmit}>
           <label htmlfor="msg">Message</label>
           <input
@@ -52,8 +98,6 @@ function App() {
             value={msg}
           />
           <button type="submit">Submit</button>
-          <AudioRecorder /> {/* Audio recording */}
-          <VideoRecorder /> {/* Video recording */}
         </form>
       </header>
     </div>
