b17586fa5f523b58127fe086a3c439fee7df24e2
Amir Zarandi <amir.h.zarandi@lawrence.edu>
2023-09-16 23:59:08 -0400

    server initialized

---
+++ server/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/palliative"
+db = SQLAlchemy(app)
+
+class Convo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.Text(), nullable = False)
+    msg = db.Column(db.String(150), nullable = False)
+
+    def __repr__(self):
+        return f"Convo: {self.msg}"
+    
+    def __init__(self, username, msg):
+        self.username = username
+        self.msg = msg
+
+def format_convo(convo):
+    return {
+        "id": convo.id,
+        "username": convo.username,
+        "msg": convo.msg
+    }
+
+@app.route('/')
+def hello():
+    return 'Hey!'
+
+# create a conversation message
+@app.route('/convos', methods = ['POST'])
+def create_convo():
+    username = request.json['username']
+    msg = request.json['msg']
+    convo = Convo(username, msg)
+    db.session.add(convo)
+    db.session.commit()
+    return format_convo(convo)
+
+# get all convos
+@app.route('/convos', methods = ['GET'])
+def get_convos():
+    convos = Convo.query.order_by(Convo.id.asc()).all()
+    convo_list = []
+    for convo in convos:
+        convo_list.append(format_convo(convo))
+    return {'convos': convo_list}
+
+# get single convo
+@app.route('/convos/<id>', methods = ['GET'])
+# def get_convo(id):
+
+
+if __name__ == '__main__':
+    app.run()
